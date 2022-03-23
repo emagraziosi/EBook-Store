bcda3cf709c13d0310531aad2c0a63a5c74d9856
Mortrak <Mortrak@host.docker.internal>
2022-03-23 14:18:25 +0100

    df

--- src/main/java/com/emanuele/ebookStore/EbookStoreApplication.java
+++ src/main/java/com/emanuele/ebookStore/EbookStoreApplication.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class EbookStoreApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Ecchelo!");
 		SpringApplication.run(EbookStoreApplication.class, args);
 	}
 
